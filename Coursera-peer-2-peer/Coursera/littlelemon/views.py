50f0afd4510e2d092b14e84ebe0627a7112c4643
Christian Louis Bolea <bolea2133@gmail.com>
2026-03-07 21:12:42 +0800

    haha tapos na
    ge

--- Coursera-peer-2-peer/Coursera/littlelemon/views.py
+++ Coursera-peer-2-peer/Coursera/littlelemon/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Menu
 
-# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def menu(request):
+    menu_data = Menu.objects.all()
+    main_data = {"menu": menu_data}
+    return render(request, 'menu.html', main_data)
+
+def display_menu_item(request, pk=None): 
+    if pk: 
+        menu_item = Menu.objects.get(pk=pk) 
+    else: 
+        menu_item = "" 
+    return render(request, 'menu_item.html', {"menu_item": menu_item})
+
+def about(request):
+    return render(request, 'about.html')
+
+def book(request):
+    return render(request, 'book.html')
