2e9afa1d87b61fc9dde01755bdff62caca115ab9
Christian Louis Bolea <bolea2133@gmail.com>
2026-03-27 15:37:02 +0800

    2

--- SemiFinalExam/RetailProject/RetailProject/settings.py
+++ SemiFinalExam/RetailProject/RetailProject/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ordering',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -115,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_URL = '/admin/login/'
